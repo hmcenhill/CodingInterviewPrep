1ad5c3ebc005a282b41d1d2ca597a865ecd06505
Hermann McEnhill <hmcenhill@gmail.com>
2022-04-05 14:12:59 -0400

    vs boilerplate and gitignore

---
+++ CodingInterviewPrep/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodingInterviewPrep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
